6a4a144364f56a74f9f5eeb0805a2e219b8d357e
DreamLose <1083531814@qq.com>
2020-11-19 09:22:50 +0800

    udp协议的使用

--- Day16Socket Udp/时间服务器Demo/ntp_client.py
+++ Day16Socket Udp/时间服务器Demo/ntp_client.py
@@ -1,6 +1,6 @@
 from socket import *
 
-ip_port = ('127.0.0.1', 8000)
+ip_port = ('127.0.0.1', 8080)
 buffer_size = 1024
 # SOCK_DGRAM 数据报  基于udp
 udp_client = socket(AF_INET, SOCK_DGRAM)
@@ -8,5 +8,5 @@ while True:
     msg = input(">>>:").strip()
     udp_client.sendto(msg.encode('utf-8'),ip_port)
     data,addr = udp_client.recvfrom(buffer_size)
-    print(data.decode('utf8'))
+    print('ntp服务器的标准时间是-->',data.decode('utf8'))
 udp_client.close()
--- Day16Socket Udp/时间服务器Demo/ntp_service.py
+++ Day16Socket Udp/时间服务器Demo/ntp_service.py
@@ -1,5 +1,6 @@
 from socket import *
-ip_port = ('127.0.0.1',8000)
+import time
+ip_port = ('127.0.0.1',8080)
 buffer_size = 1024
 # SOCK_DGRAM 数据报  基于udp
 udp_service = socket(AF_INET,SOCK_DGRAM)
@@ -7,6 +8,7 @@ udp_service.bind(ip_port)
 
 while True:
     data,addr = udp_service.recvfrom(buffer_size)
-    print(data)
-    udp_service.sendto(data.upper(),addr)
+        # print(data)
+    back_time = time.strftime("%Y-%m-%d %X")
+    udp_service.sendto(back_time.encode('utf-8'),addr)
 udp_service.close()
